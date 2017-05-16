8caa783ff7c47a50458f6ad516b73b0959edd95e
y.kobayashi <baco.16g@gmail.com>
2017-05-16 23:21:49 +0900

    initial commit

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -7,4 +7,13 @@ import '../stylus/app.styl'
 //   $('body').html('Hello World!');
 // });
 
-console.log('RxJS Boiler Running...');
+console.log('RxJS Boiler Running...')
+
+const button = $('button')
+Rx.Observable
+  .fromEvent(button, 'click')
+  .subscribe(
+    (event: MouseEvent) => {
+      console.log(`${(event.target as HTMLButtonElement).textContent} is clicked!!`)
+    }
+  )
